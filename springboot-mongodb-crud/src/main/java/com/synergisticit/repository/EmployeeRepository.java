c12d54f6d28c1feb3e11b9e499625908d4df21f4
Luyin Huang <35677157+Louis256@users.noreply.github.com>
2024-06-13 16:29:35 -0400

    Add files via upload

---
+++ springboot-mongodb-crud/src/main/java/com/synergisticit/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.synergisticit.repository;
+
+import com.synergisticit.model.Employee;
+import com.synergisticit.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EmployeeRepository extends MongoRepository<Employee, Long>{
+
+}
